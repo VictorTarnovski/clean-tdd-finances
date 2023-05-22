60a982820332db95c2439417d21fa526f373b56b
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-05-22 19:32:40 -0300

    feat: ensure BankAccount Controller returns 400 if account number is not an integer

--- src/presentation/controllers/bank-account.ts
+++ src/presentation/controllers/bank-account.ts
@@ -1,3 +1,4 @@
+import { InvalidParamError } from "../errors/invalid-param-erros"
 import { MissingParamError } from "../errors/missing-param-error"
 import { BadRequest } from "../helpers/http-helper"
 import { HttpRequest, HttpResponse } from "../protocols/http"
@@ -9,6 +10,9 @@ export class BankAccountController {
             if(!httpRequest.body[field]) {
                 return BadRequest(new MissingParamError(field))
             }
+        }
+        if(typeof httpRequest.body.number !== 'number') {
+            return BadRequest(new InvalidParamError('number'))
         }
         return {
             statusCode: 200,
