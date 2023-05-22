474f650c555a207967700402772767fd777a779d
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-05-22 19:32:50 -0300

    test: ensure BankAccount Controller returns 400 if account number is not an integer

--- src/presentation/controllers/bank-account.spec.ts
+++ src/presentation/controllers/bank-account.spec.ts
@@ -1,5 +1,6 @@
 import { BankAccountController } from './bank-account'
 import { MissingParamError } from '../errors/missing-param-error'
+import { InvalidParamError } from '../errors/invalid-param-erros'
 
 describe('BankAccount Controller', () => {
 
@@ -21,5 +22,15 @@ describe('BankAccount Controller', () => {
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new MissingParamError('currency'))
+    })
+
+    test('Should return 400 if account number is not an integer', () => {
+        const sut = new BankAccountController()
+        const httpRequest = {
+            body: { number: '1', currency: 'USD' }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new InvalidParamError('number'))
     })
 })
