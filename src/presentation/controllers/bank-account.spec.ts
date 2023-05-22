e388448880a25638a649f0cb5423d12261195350
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-05-22 14:44:06 -0300

    test: ensure BankAccount controller return an error if no number is provided

--- src/presentation/controllers/bank-account.spec.ts
+++ src/presentation/controllers/bank-account.spec.ts
@@ -5,9 +5,10 @@ describe('BankAccount Controller', () => {
     test('Should return 400 if no number is provided', () => {
         const sut = new BankAccountController()
         const httpRequest = {
-            body: { number: 1 }
+            body: { }
         }
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new Error('Missing param: number'))
     })
 })
