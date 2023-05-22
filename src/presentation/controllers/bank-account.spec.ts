8d9b2970de04199b0c03b902deb83594e2a3629f
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-05-22 19:20:12 -0300

    test: add currency field to test

--- src/presentation/controllers/bank-account.spec.ts
+++ src/presentation/controllers/bank-account.spec.ts
@@ -6,7 +6,7 @@ describe('BankAccount Controller', () => {
     test('Should return 400 if no number is provided', () => {
         const sut = new BankAccountController()
         const httpRequest = {
-            body: { }
+            body: { currency: 'USD' }
         }
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
