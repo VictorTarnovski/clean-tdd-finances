b68a7bdfb9458cc9b78011b70ab5fa9c4589280a
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-05-22 19:43:13 -0300

    feat: ensure BankAccount returns 500 if and unexpected error occurs

--- src/presentation/controllers/bank-account.ts
+++ src/presentation/controllers/bank-account.ts
@@ -1,21 +1,29 @@
 import { InvalidParamError } from "../errors/invalid-param-erros"
 import { MissingParamError } from "../errors/missing-param-error"
+import { ServerError } from "../errors/server-error"
 import { BadRequest } from "../helpers/http-helper"
 import { HttpRequest, HttpResponse } from "../protocols/http"
 
 export class BankAccountController {
     handle(httpRequest: HttpRequest): HttpResponse {
-        const requiredFields: string[] = ['number', 'currency']
-        for(const field of requiredFields) {   
-            if(!httpRequest.body[field]) {
-                return BadRequest(new MissingParamError(field))
+        try { 
+            const requiredFields: string[] = ['number', 'currency']
+            for(const field of requiredFields) {   
+                if(!httpRequest.body[field]) {
+                    return BadRequest(new MissingParamError(field))
+                }
             }
-        }
-        if(typeof httpRequest.body.number !== 'number') return BadRequest(new InvalidParamError('number'))
-        if(typeof httpRequest.body.currency !== 'string') return BadRequest(new InvalidParamError('currency'))
-        return {
-            statusCode: 200,
-            body: { message: 'OK' }
+            if(typeof httpRequest.body.number !== 'number') return BadRequest(new InvalidParamError('number'))
+            if(typeof httpRequest.body.currency !== 'string') return BadRequest(new InvalidParamError('currency'))
+            return {
+                statusCode: 200,
+                body: { message: 'OK' }
+            }   
+        } catch (error) {
+            return {
+                statusCode: 500,
+                body: new ServerError()
+            }
         }
     }
 }
