8507cacf7522692440c5716a131f6bc56f223e77
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-05-22 19:16:41 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/bank-account.ts
+++ src/presentation/controllers/bank-account.ts
@@ -1,19 +1,14 @@
 import { MissingParamError } from "../errors/missing-param-error"
+import { BadRequest } from "../helpers/http-helper"
 import { HttpRequest, HttpResponse } from "../protocols/http"
 
 export class BankAccountController {
     handle(httpRequest: HttpRequest): HttpResponse {
         if(!httpRequest.body.number) {
-            return { 
-                statusCode: 400, 
-                body: new MissingParamError('number') 
-            }
+            return BadRequest(new MissingParamError('number'))
         }
         if(!httpRequest.body.currency) {
-            return { 
-                statusCode: 400, 
-                body: new MissingParamError('currency') 
-            }
+            return BadRequest(new MissingParamError('currency'))
         }
         return {
             statusCode: 200,
