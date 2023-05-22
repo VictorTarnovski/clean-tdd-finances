84737291be4a75c8524be8694adfa778f1dca4fc
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-05-22 20:13:11 -0300

    feat: ensure BankAccount Controller call AddBankAccount with correct values

--- src/presentation/controllers/bank-account.ts
+++ src/presentation/controllers/bank-account.ts
@@ -1,9 +1,14 @@
+import { AddBankAccount } from "../../domain/use-cases/add-bank-account"
 import { MissingParamError, InvalidParamError, ServerError } from "../errors"
 import { badRequest } from "../helpers/http-helper"
 import { HttpRequest, HttpResponse } from "../protocols/http"
 
 export class BankAccountController {
-    handle(httpRequest: HttpRequest): HttpResponse {
+    addBankAccount: AddBankAccount
+    constructor(addBankAccount: AddBankAccount) {
+        this.addBankAccount = addBankAccount
+    }
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         try { 
             const requiredFields: string[] = ['number', 'currency']
             for(const field of requiredFields) {   
@@ -14,6 +19,7 @@ export class BankAccountController {
             const { number, currency } = httpRequest.body
             if(typeof number !== 'number') return badRequest(new InvalidParamError('number'))
             if(typeof currency !== 'string') return badRequest(new InvalidParamError('currency'))
+            const bankAccount = await this.addBankAccount.add(httpRequest.body)
             return {
                 statusCode: 200,
                 body: { message: 'OK' }
