806e232cbb53cfacb16c95851ee2256c5a2240c3
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-05-22 14:54:44 -0300

    test: ensure BankAccount controller return an error if no currency is provided

--- src/presentation/controllers/bank-account.spec.ts
+++ src/presentation/controllers/bank-account.spec.ts
@@ -11,4 +11,14 @@ describe('BankAccount Controller', () => {
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new Error('Missing param: number'))
     })
+
+    test('Should return 400 if no currency is provided', () => {
+        const sut = new BankAccountController()
+        const httpRequest = {
+            body: { number: 1 }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new Error('Missing param: currency'))
+    })
 })
