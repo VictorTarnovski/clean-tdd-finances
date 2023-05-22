0dbc3f29c0416ea9136b39f87db34f4e89bf7784
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-05-22 11:56:28 -0300

    test: ensure FirebaseAccountRepository implements AddBankAccountRepository

---
+++ src/infra/db/firebase/account-repository/account.spec.ts
@@ -0,0 +1,36 @@
+import { HttpService, RequestModel, ResponseModel } from "../../../http/protocols/http-service"
+import { FirebaseAccountRepository } from "./account"
+
+describe('FirebaseAccountRepository', () => {
+
+    const makeHttpServiceStub = () => {
+        class HttpServiceStub implements HttpService {
+            async post({ url, requestBody, requestParams, requestHeaders }: RequestModel): Promise<ResponseModel> {
+                return { statusCode: 200 }
+            }
+        }
+        return new HttpServiceStub()
+    }
+
+    const makeSut = () => {
+        const httpServiceStub = makeHttpServiceStub()
+        const sut = new FirebaseAccountRepository(httpServiceStub)
+        return {
+            sut,
+            httpServiceStub
+        }
+    }
+    
+    test('Should return an account on sucess', async () => {
+        const { sut, httpServiceStub } = makeSut()
+        jest.spyOn(httpServiceStub, 'post').mockImplementationOnce(async () => {
+            return { statusCode: 200, body: '1' }
+        })
+        const bankAccount = await sut.add({ 
+            number: 123 
+        })
+        expect(bankAccount).toBeTruthy()
+        expect(bankAccount.number).toBe(123)
+        expect(bankAccount.id).toBe('1')
+    }) 
+})
