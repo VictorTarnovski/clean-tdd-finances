263ad151e48080dc7ff1e71abefe07e3d6df8330
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-05-22 11:56:23 -0300

    feat: ensure FirebaseAccountRepository implements AddBankAccountRepository

---
+++ src/infra/db/firebase/account-repository/account.ts
@@ -0,0 +1,19 @@
+import { AddBankAccountRepository } from "../../../../data/protocols/add-bank-account-repository";
+import { BankAccountModel } from "../../../../domain/models/bank-account";
+import { AddBankAccountModel } from "../../../../domain/use-cases/add-bank-account";
+import { HttpService } from "../../../http/protocols/http-service";
+import { FirebaseHelper } from "./helpers/firebase-helper";
+
+export class FirebaseAccountRepository implements AddBankAccountRepository {
+    firebaseHelper: FirebaseHelper
+    httpService: HttpService
+    constructor(httpService: HttpService) {
+        this.httpService = httpService
+        this.firebaseHelper = new FirebaseHelper(this.httpService)
+    }
+    async add(bankAccountData: AddBankAccountModel): Promise<BankAccountModel> {
+        const bankAccountReference = this.firebaseHelper.getReference('bank-accounts')
+        const { id } = await this.firebaseHelper.addChild(bankAccountReference, bankAccountData)
+        return Object.assign({}, {id: id}, bankAccountData)
+    }
+}
