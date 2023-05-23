9ebed37bc8dce0ffd85764b506c59c8eae044969
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-05-23 13:33:09 -0300

    test: ensure LogController Decorator calls controller and returns it httpResponse

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,43 @@
+import { LogControllerDecorator } from "./log"
+import { Controller, HttpRequest, HttpResponse } from "../../presentation/protocols"
+import { ok } from "../../presentation/helpers/http-helper"
+
+describe('LogController Decorator',  () => {
+
+    interface SutTypes {
+        sut: LogControllerDecorator,
+        controllerStub: Controller
+    }
+
+    const makeController = (): Controller => {
+        class ControllerStub implements Controller {
+            async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+                return ok('OK')
+            }
+        }
+        return new ControllerStub()
+    }
+
+    const makeSut = (): SutTypes => {
+        const controllerStub = makeController()
+        const sut = new LogControllerDecorator(controllerStub)
+        return {
+            sut,
+            controllerStub
+        }
+    }
+
+    test('Should call controller handle',  async () => {
+        const { sut, controllerStub } = makeSut()
+        const handleSpy = jest.spyOn(controllerStub, 'handle')
+        const httpRequest = {
+            body: {
+                number: 1,
+                currency: 'USD'
+            }
+        }
+        await sut.handle(httpRequest)
+        expect(handleSpy).toBeCalledWith(httpRequest)
+    })
+
+})
