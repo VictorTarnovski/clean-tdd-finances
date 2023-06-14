9a38ffa5e6f7ed81c5dc6b9be37fb25e000d5ad9
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-06-14 04:19:07 -0300

    test: ensure BankCard Controller returns 400 if no flag is provided

--- src/presentation/controllers/bank-cards/bank-card.spec.ts
+++ src/presentation/controllers/bank-cards/bank-card.spec.ts
@@ -1,9 +1,10 @@
 import { BankCardController } from "./bank-card"
 
 import { HttpRequest } from "../../protocols"
+import { MissingParamError, badRequest } from "../bank-account/bank-account-protocols"
 describe('BankCard Controller', () => {
                     
-    test('Should return 400 if no number if provided', async () => {
+    test('Should return 400 if no number is provided', async () => {
         const sut = new BankCardController()
         const httpRequest: HttpRequest = {
             body: {
@@ -12,7 +13,20 @@ describe('BankCard Controller', () => {
             }
         }
         const httpResponse = await sut.handle(httpRequest)
-        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse).toEqual(badRequest( new MissingParamError('number')))
+                    
+    })
+
+    test('Should return 400 if no flag is provided', async () => {
+        const sut = new BankCardController()
+        const httpRequest: HttpRequest = {
+            body: {
+                number: 1,
+                expiresAt: "2027-02-01",
+            }
+        }
+        const httpResponse = await sut.handle(httpRequest)
+        expect(httpResponse).toEqual(badRequest( new MissingParamError('flag')))
                     
     })
 })
