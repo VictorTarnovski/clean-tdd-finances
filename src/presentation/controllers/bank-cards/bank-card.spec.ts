8fee5f82817b287f486c94ab0e758e100eeb2e2d
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-06-14 05:32:03 -0300

    test: ensure BankCard Controller returns 500 if AddBankCard throws

--- src/presentation/controllers/bank-cards/bank-card.spec.ts
+++ src/presentation/controllers/bank-cards/bank-card.spec.ts
@@ -2,7 +2,7 @@ import { BankCardController } from "./bank-card"
 
 import { HttpRequest } from "../../protocols"
 import { badRequest } from '../../helpers/http-helper'
-import { MissingParamError, InvalidParamError } from '../../errors'
+import { MissingParamError, InvalidParamError, ServerError } from '../../errors'
 import { AddBankCard, AddBankCardModel } from '../../../domain/use-cases/add-bank-card'
 import { BankCardModel } from "../../../domain/models/bank-card"
 
@@ -115,5 +115,16 @@ describe('BankCard Controller', () => {
         const httpResponse = await sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(200)
         expect(httpResponse.body).toEqual({ id: 'valid_id', number: 1, flag: 'valid_flag', expiresAt: '2023-06-14' })
+    })
+
+    test('Should return 500 if AddBankCard throws', async () => {
+        const { sut, addBankCardStub } = makeSut()
+        const httpRequest = {
+            body: { number: 1, flag: 'valid_flag', expiresAt: '2023-06-14' }
+        }
+        jest.spyOn(addBankCardStub, 'add').mockRejectedValueOnce(new Error())
+        const httpResponse = await sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(500)
+        expect(httpResponse.body).toEqual(new ServerError('Internal Server Error'))
     })
 })
