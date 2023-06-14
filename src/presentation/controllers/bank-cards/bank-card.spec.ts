6d03a0a84badce99f3636b6ba5386a9b652bd1cd
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-06-14 04:26:05 -0300

    test: ensure BankCard Controller returns 400 if card number is not an integer

--- src/presentation/controllers/bank-cards/bank-card.spec.ts
+++ src/presentation/controllers/bank-cards/bank-card.spec.ts
@@ -1,7 +1,9 @@
 import { BankCardController } from "./bank-card"
 
 import { HttpRequest } from "../../protocols"
-import { MissingParamError, badRequest } from "../bank-account/bank-account-protocols"
+import { badRequest } from '../../helpers/http-helper'
+import { MissingParamError, InvalidParamError } from '../../errors'
+
 describe('BankCard Controller', () => {
                     
     test('Should return 400 if no number is provided', async () => {
@@ -35,11 +37,21 @@ describe('BankCard Controller', () => {
         const httpRequest: HttpRequest = {
             body: {
                 number: 1,
-                flag: 'any'
+                flag: 'any_flag'
             }
         }
         const httpResponse = await sut.handle(httpRequest)
         expect(httpResponse).toEqual(badRequest( new MissingParamError('expiresAt')))
                     
+    })
+
+    test('Should return 400 if card number is not an integer', async () => {
+        const sut = new BankCardController()
+        const httpRequest = {
+            body: { number: '1', flag: 'any_flag', expiresAt: '2027-06-14' }
+        }
+        const httpResponse = await sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new InvalidParamError('number'))
     })
 })
