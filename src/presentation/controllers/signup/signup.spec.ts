527aac72fad9244c29333fe884e085414a0f74eb
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-06-16 20:17:01 -0300

    style: change test order to improve readability

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -92,18 +92,20 @@ describe('SignUp Controller', () => {
         expect(httpResponse).toEqual(badRequest(new InvalidParamError('passwordConfirmation')))
     })
 
-    test('Should return 200 if valid data is provided', async () => {
-        const { sut } = makeSut()
+    test('Should call emailValidator with correct email', async () => {
+        const { sut, emailValidatorStub } = makeSut()
+        const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
         const httpRequest = {
             body: {
-                email: 'any_mail@mail.com',
+                email: 'valid_mail@mail.com',
                 name: 'any_name',
                 password: 'any_password',
                 passwordConfirmation: 'any_password'
             }
         }
         const httpResponse = await sut.handle(httpRequest)
-        expect(httpResponse.statusCode).toBe(200)
+        expect(isValidSpy).toHaveBeenCalledTimes(1)
+        expect(isValidSpy).toBeCalledWith('valid_mail@mail.com')
     })
 
     test('Should return 400 if an invalid email is provided', async () => {
@@ -121,22 +123,6 @@ describe('SignUp Controller', () => {
         expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')))
     })
 
-    test('Should call emailValidator with correct email', async () => {
-        const { sut, emailValidatorStub } = makeSut()
-        const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
-        const httpRequest = {
-            body: {
-                email: 'valid_mail@mail.com',
-                name: 'any_name',
-                password: 'any_password',
-                passwordConfirmation: 'any_password'
-            }
-        }
-        const httpResponse = await sut.handle(httpRequest)
-        expect(isValidSpy).toHaveBeenCalledTimes(1)
-        expect(isValidSpy).toBeCalledWith('valid_mail@mail.com')
-    })
-
     test('Should return 500 if emailValidator throws', async () => {
         const { sut, emailValidatorStub } = makeSut()
         const mockedError = new Error()
@@ -152,4 +138,19 @@ describe('SignUp Controller', () => {
         const httpResponse = await sut.handle(httpRequest)
         expect(httpResponse).toEqual(serverError(mockedError))
     })
+
+    test('Should return 200 if valid data is provided', async () => {
+        const { sut } = makeSut()
+        const httpRequest = {
+            body: {
+                email: 'any_mail@mail.com',
+                name: 'any_name',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+            }
+        }
+        const httpResponse = await sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(200)
+    })
+
 })
