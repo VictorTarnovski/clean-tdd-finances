9dd0b4e130e805200e770aa80feee0ea5eedef2a
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-07-07 13:42:51 -0300

    refactor: move sut creation to a factory method

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -2,15 +2,29 @@ import { ValidationComposite } from "./validation-composite"
 import { InvalidParamError } from "../../errors"
 import { Validation } from "./validation"
 
-class ValidationStub implements Validation {
-    validate(input: any): Error | null {
-        return new InvalidParamError('any_field')
+const makeValidationStub = (): Validation => {
+    class ValidationStub implements Validation {
+        validate(input: any): Error | null {
+            return new InvalidParamError('any_field')
+        }
     }
+    return new ValidationStub()
+}
+
+interface SutTypes {
+    sut: ValidationComposite
+    validationStub: Validation
+}
+
+const makeSut = (): SutTypes => {
+    const validationStub = makeValidationStub()
+    const sut = new ValidationComposite([validationStub])
+    return { sut, validationStub }
 }
 
 describe('ValidationComposite', () => {
    test('Should return an Error if any validation fails', () => {
-    const sut = new ValidationComposite([new ValidationStub()])
+    const { sut } = makeSut()
     const error = sut.validate({ any_field: 'any_value'})
     expect(error).toEqual(new InvalidParamError('any_field'))
    })
