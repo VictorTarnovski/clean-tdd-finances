61079c8a23f35cbc181b6cf8688a7295220b196b
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-07-07 16:33:10 -0300

    feat: ensure SupportedValuesValidation returns a InvalidParamError if validation fails

---
+++ src/presentation/helpers/validators/supported-values-validation/supported-values-validation.ts
@@ -0,0 +1,23 @@
+import { InvalidParamError } from "../../../errors"
+import { Validation } from "../validation"
+
+export class SupportedValuesValidation implements Validation {
+    private readonly fieldName: string
+    private readonly supportedValues: string[]
+    constructor(fieldName: string, supportedValues: string[]) {
+        this.fieldName = fieldName
+        this.supportedValues = supportedValues
+    }
+    validate(input: any): Error | null {
+        let matches: number = 0
+        for(const value of this.supportedValues) {
+            if(input[this.fieldName] === value) {
+                matches = matches + 1
+            }
+        }
+        if(matches !== this.supportedValues.length) {
+            return new InvalidParamError(this.fieldName)
+        }
+        return null
+    }
+}
