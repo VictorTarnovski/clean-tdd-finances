8537170a5d4e440515fb1cfbac4760008ff49e4c
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-07-07 13:33:15 -0300

    style: move each validation to it respective folder

--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -1,9 +1,9 @@
 import { ValidationComposite } from "../../presentation/helpers/validators/validation-composite"
-import { RequiredFieldValidation } from "../../presentation/helpers/validators/required-field-validation"
+import { RequiredFieldValidation } from "../../presentation/helpers/validators/required-field-validation/required-field-validation"
 import { Validation } from "../../presentation/helpers/validators/validation"
-import { CompareFieldsValidation } from "../../presentation/helpers/validators/compare-fields-validation"
+import { CompareFieldsValidation } from "../../presentation/helpers/validators/compare-fields-validation/compare-fields-validation"
 import { EmailValidatorAdapter } from "../../utils/email-validator-adapter"
-import { EmailValidation } from "../../presentation/helpers/validators/email-validation"
+import { EmailValidation } from "../../presentation/helpers/validators/email-validation/email-validation"
 
 
 export const makeSignUpValidation = (): ValidationComposite => {
--- src/presentation/helpers/validators/email-validation/email-validation.ts
+++ src/presentation/helpers/validators/email-validation/email-validation.ts
@@ -1,6 +1,6 @@
-import { InvalidParamError } from "../../errors"
-import { EmailValidator } from "../../protocols/email-validator"
-import { Validation } from "./validation"
+import { InvalidParamError } from "../../../errors"
+import { EmailValidator } from "../../../protocols/email-validator"
+import { Validation } from "../validation"
 
 export class EmailValidation implements Validation {
     private readonly fieldName: string
