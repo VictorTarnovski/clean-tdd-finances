b95df78c29d70ddd7155d6e61f35abdac84f2518
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-07-07 16:37:56 -0300

    feat: ensure makeBankAccountValidation calls ValidationComposite with all validations

---
+++ src/main/factories/bank-account-validation.ts
@@ -0,0 +1,15 @@
+import { ValidationComposite } from "../../presentation/helpers/validators/validation-composite"
+import { RequiredFieldValidation } from "../../presentation/helpers/validators/required-field-validation/required-field-validation"
+import { Validation } from "../../presentation/helpers/validators/validation"
+import { SupportedValuesValidation } from "../../presentation/helpers/validators/supported-values-validation/supported-values-validation"
+
+
+export const makeBankAccountValidation = (): ValidationComposite => {
+    const validations: Validation[] = []
+     for (const field of ['number', 'balance', 'currency']) {
+        validations.push(new RequiredFieldValidation(field))
+     }
+     const supportedCurrencies: string[] = ['USD', 'EUR', 'GBP', 'JPY', 'BRL']
+     validations.push(new SupportedValuesValidation('currency', supportedCurrencies))
+    return new ValidationComposite(validations)
+}
