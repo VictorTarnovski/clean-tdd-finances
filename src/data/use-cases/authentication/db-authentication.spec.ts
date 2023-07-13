18d3016ad916023ab67f693c7ee4c1e34418d5e2
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-07-13 17:10:22 -0300

    refactor: move mocked authentication to a factory

--- src/data/use-cases/authentication/db-authentication.spec.ts
+++ src/data/use-cases/authentication/db-authentication.spec.ts
@@ -1,6 +1,9 @@
 import { AccountModel } from "../../../domain/models/account"
+import { AuthenticationModel } from "../../../domain/use-cases/authentication"
 import { LoadAccountByEmailRepository } from "../../protocols/load-account-by-email-repository"
 import { DbAuthentication } from "./db-authentication"
+
+const makeFakeAuthentication = (): AuthenticationModel => ({email: 'any_mail@mail.com', password: 'any_password'})
 
 const makeLoadAccountByEmailRepositoryStub = (): LoadAccountByEmailRepository => {
     class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository { 
@@ -35,7 +38,7 @@ describe('DbAuthentication UseCase', () => {
    test('Should call LoadAccountByEmailRepository with correct email', async () => {
     const { sut, loadAccountByEmailRepositoryStub } = makeSut()
     const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'load')
-    await sut.auth({email: 'any_mail@mail.com', password: 'any_password'})
+    await sut.auth(makeFakeAuthentication())
     expect(loadSpy).toHaveBeenCalledWith('any_mail@mail.com')
    })
 })
