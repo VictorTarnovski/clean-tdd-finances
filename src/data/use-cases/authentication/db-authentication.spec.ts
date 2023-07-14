9a0704930f0cbea73ffdae91eaf90c21b95b572e
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-07-14 13:07:31 -0300

    test: ensure DbAuthentication thorws if TokenGenerator throws

--- src/data/use-cases/authentication/db-authentication.spec.ts
+++ src/data/use-cases/authentication/db-authentication.spec.ts
@@ -109,4 +109,11 @@ describe('DbAuthentication UseCase', () => {
     await sut.auth(makeFakeAuthentication())
     expect(generateSpy).toHaveBeenCalledWith('any_id')
    })
+
+   test('Should throw if HashComparer throws', async () => {
+    const { sut, tokenGeneratorStub } = makeSut()
+    jest.spyOn(tokenGeneratorStub, 'generate').mockRejectedValueOnce(new Error())
+    const promise = sut.auth(makeFakeAuthentication())
+    await expect(promise).rejects.toThrow()
+   })
 })
