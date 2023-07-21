7b259cfd3b5556d75c3a9e57f02c431eb3f7ce7c
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-07-21 10:14:24 -0300

    feat: ensure SignUpController calls Authentication with correct values

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -1,12 +1,12 @@
 import { HttpRequest, HttpResponse } from "../../protocols"
 import { Controller } from "../../protocols"
-import { InvalidParamError, badRequest, ok, serverError } from "../bank-account/bank-account-controller-protocols"
-import { EmailValidator } from "../../protocols/email-validator"
+import { badRequest, ok, serverError } from "../bank-account/bank-account-controller-protocols"
 import { AddAccount } from "../../../domain/use-cases/add-account"
 import { Validation } from "../../protocols/validation"
+import { Authentication } from "../../../domain/use-cases/authentication"
 
 export class SignUpController implements Controller {
-    constructor (private readonly addAccount: AddAccount, private readonly validation: Validation) {}
+    constructor (private readonly addAccount: AddAccount, private readonly validation: Validation, private readonly authentication: Authentication) {}
 
     async handle(httpRequest: HttpRequest): Promise<HttpResponse>{
         try {   
@@ -16,6 +16,9 @@ export class SignUpController implements Controller {
             }
             const { name, email, password } = httpRequest.body
             const account = await this.addAccount.add({ name, email, password })
+            if (account) {
+                await this.authentication.auth({ email: account.email, password: account.password })
+            }
             return ok(account)
         } catch (error: any) {
             return serverError(error)
