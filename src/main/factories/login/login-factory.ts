f3c8e39f2d4ecd371ae1eff0464ed8da5afd7b39
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-07-21 08:25:03 -0300

    chore: enable vscode debuging

--- src/main/factories/login/login-factory.ts
+++ src/main/factories/login/login-factory.ts
@@ -7,10 +7,7 @@ import { MongoLogRespository } from "../../../infra/db/mongodb/log/mongo-log-rep
 import { LoginController } from "../../../presentation/controllers/login/login-controller"
 import { Controller } from "../../../presentation/protocols"
 import { LogControllerDecorator } from "../../decorators/log-controller.decorator"
-import { EmailValidation, RequiredFieldValidation, ValidationComposite } from "../../../presentation/helpers/validators"
-import { EmailValidatorAdapter } from "../../adapters/validators/email-validator-adapter"
-
-const loginValidation = new ValidationComposite([new RequiredFieldValidation('email'), new RequiredFieldValidation('password'), new EmailValidation(new EmailValidatorAdapter(), 'name')])
+import { makeLoginValidation } from "./login-validation-factory"
 
 export const makeLoginController = (): Controller => {
     const salt = 12
@@ -18,7 +15,7 @@ export const makeLoginController = (): Controller => {
     const jwtAdapter = new JwtAdapter(env.jwtSecret)
     const accountRepository = new MongoAccountRepository()
     const dbAuthentication = new DbAuthentication(accountRepository, bcryptAdapter, jwtAdapter, accountRepository)
-    const loginController = new LoginController(dbAuthentication, loginValidation)
+    const loginController = new LoginController(dbAuthentication, makeLoginValidation())
     const logErrorRespotiroy = new MongoLogRespository()
     return new LogControllerDecorator(loginController, logErrorRespotiroy)
 }
