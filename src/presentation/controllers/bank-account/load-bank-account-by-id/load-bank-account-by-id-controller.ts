79477ddbea5c53734d118a03c9faa1fe03e4178f
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-07-25 16:48:50 -0300

    feat: ensure LoadBankAccountById Controller returns 200 on sucess

--- src/presentation/controllers/bank-account/load-bank-account-by-id/load-bank-account-by-id-controller.ts
+++ src/presentation/controllers/bank-account/load-bank-account-by-id/load-bank-account-by-id-controller.ts
@@ -4,7 +4,7 @@ import { ok } from "../add-bank-account/add-bank-account-controller-protocols"
 export class LoadBankAccountByIdController implements Controller {
   constructor(private readonly loadBankAccountById: LoadBankAccountById) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadBankAccountById.load(httpRequest.params.bankAccountId)
-    return ok({})
+    const bankAccount = await this.loadBankAccountById.load(httpRequest.params.bankAccountId)
+    return ok(bankAccount)
   }
 }
