31737f9c86a114ac19b40a7d53ea1e53c526fe85
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-07-25 17:13:12 -0300

    feat: ensure LoadBankAccountById Controller returns 500 if LoadBankAccountById thorws

--- src/presentation/controllers/bank-account/load-bank-account-by-id/load-bank-account-by-id-controller.ts
+++ src/presentation/controllers/bank-account/load-bank-account-by-id/load-bank-account-by-id-controller.ts
@@ -1,11 +1,15 @@
 import { Controller, HttpRequest, HttpResponse, LoadBankAccountById } from "./load-bank-account-by-id-controller-protocols"
-import { ok, notFound } from "../add-bank-account/add-bank-account-controller-protocols"
+import { ok, notFound, serverError } from "../add-bank-account/add-bank-account-controller-protocols"
 
 export class LoadBankAccountByIdController implements Controller {
-  constructor(private readonly loadBankAccountById: LoadBankAccountById) {}
+  constructor(private readonly loadBankAccountById: LoadBankAccountById) { }
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const bankAccount = await this.loadBankAccountById.load(httpRequest.params.bankAccountId)
-    if(!bankAccount) return notFound('bankAccount')
-    return ok(bankAccount)
+    try {
+      const bankAccount = await this.loadBankAccountById.load(httpRequest.params.bankAccountId)
+      if (!bankAccount) return notFound('bankAccount')
+      return ok(bankAccount)
+    } catch (error: any) {
+      return serverError(error)
+    }
   }
 }
