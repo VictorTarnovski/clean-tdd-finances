07e1e93b67fc7132b484abdc0e760251b7c49700
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-07-25 17:55:04 -0300

    feat: ensure MongoBankAccountRepository returns an account on loadByID success

--- src/infra/db/mongodb/bank-account/mongo-bank-account-repository.ts
+++ src/infra/db/mongodb/bank-account/mongo-bank-account-repository.ts
@@ -1,14 +1,22 @@
+import { ObjectId } from "mongodb"
 import { AddBankAccountRepository } from "../../../../data/protocols/db/bank-account/add-bank-account-repository"
+import { LoadBankAccountByIdRepository } from "../../../../data/protocols/db/bank-account/load-bank-account-by-id-repository"
 import { BankAccountModel } from "../../../../domain/models/bank-account"
 import { AddBankAccountModel } from "../../../../domain/use-cases/add-bank-account"
 import mongoHelper from "../mongo-helper"
 
-export class MongoBankAccountRepository implements AddBankAccountRepository {
+export class MongoBankAccountRepository implements AddBankAccountRepository, LoadBankAccountByIdRepository {
     async add(bankAccountData: AddBankAccountModel): Promise<BankAccountModel> {
         const bankAccountsCollection = await mongoHelper.getCollection('bank-accounts')
 
         const { insertedId } = await bankAccountsCollection.insertOne(Object.assign({}, bankAccountData, { cards: [] }))
         const mongoBankAccount = await bankAccountsCollection.findOne(insertedId)
+        const bankAccount = mongoHelper.map(mongoBankAccount)
+        return bankAccount
+    }
+    async loadById(id: string): Promise<BankAccountModel | null> {
+        const bankAccountsCollection = await mongoHelper.getCollection('bank-accounts')
+        const mongoBankAccount = await bankAccountsCollection.findOne({ _id: new ObjectId(id) })
         const bankAccount = mongoHelper.map(mongoBankAccount)
         return bankAccount
     }
