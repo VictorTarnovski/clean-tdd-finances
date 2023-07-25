6aa6a5e0d8e6e6a31b98e0928070fa6162c80a21
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-07-25 16:43:50 -0300

    refactor: move sut creation to a factory helper method

--- src/presentation/controllers/bank-account/load-bank-account-by-id/load-bank-account-by-id-controller.spec.ts
+++ src/presentation/controllers/bank-account/load-bank-account-by-id/load-bank-account-by-id-controller.spec.ts
@@ -1,6 +1,5 @@
-import { BankAccountModel } from "../../../../domain/models/bank-account"
+import { BankAccountModel, LoadBankAccountById } from "./load-bank-account-by-id-controller-protocols"
 import { LoadBankAccountByIdController } from "./load-bank-account-by-id-controller"
-import { LoadBankAccountById } from "../../../../domain/use-cases/load-bank-account-by-id"
 
 const makeBankAccount = (): BankAccountModel => ({
   id: 'valid_id',
@@ -10,16 +9,33 @@ const makeBankAccount = (): BankAccountModel => ({
   cards: []
 })
 
+const makeLoadBankAccountByIdStub = (): LoadBankAccountById => {
+  class LoadBankAccountByIdStub implements LoadBankAccountById {
+    async load(): Promise<BankAccountModel | null> {
+      return makeBankAccount()
+    }
+  }
+  return new LoadBankAccountByIdStub()
+}
+
+interface SutTypes {
+  sut: LoadBankAccountByIdController
+  loadBankAccountByIdStub: LoadBankAccountById
+}
+
+const makeSut = (): SutTypes => {
+  const loadBankAccountByIdStub = makeLoadBankAccountByIdStub()
+  const sut = new LoadBankAccountByIdController(loadBankAccountByIdStub)
+  return {
+    sut,
+    loadBankAccountByIdStub
+  }
+}
+
 describe('LoadBankAccountById Controller', () => {
   test('Should call LoadBankAccountById with correct id', async () => {
-    class LoadBankAccountByIdStub implements LoadBankAccountById {
-      async load(): Promise<BankAccountModel | null> {
-        return makeBankAccount()
-      }
-    }
-    const loadBankAccountByIdStub = new LoadBankAccountByIdStub()
+    const { sut, loadBankAccountByIdStub } = makeSut()
     const loadByIdSpy = jest.spyOn(loadBankAccountByIdStub, 'load')
-    const sut = new LoadBankAccountByIdController(loadBankAccountByIdStub)
     await sut.handle({ params: { bankAccountId: 'valid_id'}})
     expect(loadByIdSpy).toHaveBeenCalledWith('valid_id')
   })
