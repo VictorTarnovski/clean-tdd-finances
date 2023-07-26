317c3a95053736510eff996f55461b21be985307
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-07-26 11:14:01 -0300

    refactor: move auth to separated files

--- src/main/routes/bank-acount-routes.ts
+++ src/main/routes/bank-acount-routes.ts
@@ -3,26 +3,25 @@ import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeAddBankAccountController } from '../factories/controllers/bank-account/add-bank-account/add-bank-account-controller-factory'
 import { makeBankCardController } from '../factories/controllers/bank-card/add-bank-card/add-bank-card-controller-factory'
 import { makeLogControllerDecorator } from '../factories/decorators/log-controller-decorator-factory'
-import { adaptMiddleware } from '../adapters/express/express-middleware-adapter'
-import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
 import { makeLoadBankAccountByIdController } from '../factories/controllers/bank-account/load-bank-account-by-id/load-bank-account-by-id-controller-factory'
+import { auth } from '../middlewares/auth'
 
 export default (router: Router): void => {
   router.post('/bank-accounts',
-    adaptMiddleware(makeAuthMiddleware()),
+    auth,
     adaptRoute(makeLogControllerDecorator(makeAddBankAccountController()
     )
     )
   )
 
   router.post('/bank-accounts/:bankAccountId/bank-cards',
-    adaptMiddleware(makeAuthMiddleware()),
+    auth,
     adaptRoute(makeLogControllerDecorator(makeBankCardController()
     )
     )
   )
   router.get('/bank-accounts/:bankAccountId',
-    adaptMiddleware(makeAuthMiddleware()),
+    auth,
     adaptRoute(makeLogControllerDecorator(makeLoadBankAccountByIdController()
     )
     )
