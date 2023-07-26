3d3b420d2beca670d1cc53553a0aaa6902c329ad
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-07-26 14:36:28 -0300

    chore: make it possible to run the app locally

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 
 export default {
-    mongo_url: process.env.MONGO_URI || 'mongodb://mongo:27017/clean-tdd-finances',
+    mongo_url: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/clean-tdd-finances',
     port: process.env.PORT || 5050,
     jwtSecret: process.env.JWT_SECRET || 'xtebc0zwKYa714q8K50n'
 }
