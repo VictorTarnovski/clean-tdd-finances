4e8301fd1adc57f457bdf9d53111ecc97ad9561e
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-07-27 18:43:28 -0300

    refactor: add factory sufix to factory files

--- src/main/factories/controllers/bank-account/load-bank-account-by-id/load-bank-account-by-id-controller-factory.ts
+++ src/main/factories/controllers/bank-account/load-bank-account-by-id/load-bank-account-by-id-controller-factory.ts
@@ -1,6 +1,6 @@
 import { LoadBankAccountByIdController } from "@/presentation/controllers/bank-account/load-bank-account-by-id/load-bank-account-by-id-controller"
 import { Controller } from "@/presentation/protocols"
-import { makeDbLoadBankAccountById } from "@/main/factories/use-cases/bank-account/load-bank-account-by-id/load-bank-account-by-id"
+import { makeDbLoadBankAccountById } from "@/main/factories/use-cases/bank-account/load-bank-account-by-id/load-bank-account-by-id-factory"
 import { makeDbLoadAccountById } from "@/main/factories/use-cases/account/load-account-by-id/db-load-account-by-token-factory"
 import { LogControllerDecorator } from "@/main/decorators/log-controller.decorator"
 import { MongoLogRespository } from "@/infra/db/mongodb/log/mongo-log-repository"
