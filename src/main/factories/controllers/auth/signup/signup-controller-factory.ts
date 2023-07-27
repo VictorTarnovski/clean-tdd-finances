dde58f6b9a8fd5820bb507ba07002fc503527d14
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-07-27 18:43:51 -0300

    refactor: add factory sufix to factory files

--- src/main/factories/controllers/auth/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/auth/signup/signup-controller-factory.ts
@@ -1,7 +1,7 @@
 import { SignUpController } from "@/presentation/controllers/auth/signup/signup-controller"
 import { Controller } from "@/presentation/protocols"
 import { makeSignUpValidation } from "./signup-validation-factory"
-import { makeDbAuthentication } from "@/main/factories/use-cases/account/authentication/db-authentication-factory"
+import { makeDbAuthentication } from "@/main/factories/use-cases/authentication/db-authentication-factory"
 import { makeDbAddAccount } from "@/main/factories/use-cases/account/add-account/db-add-account-factory"
 
 
