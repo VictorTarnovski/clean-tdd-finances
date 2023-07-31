6b1410f650ea23b72a9d4f273e7112d82d190824
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-07-31 09:20:18 -0300

    feat: ensure AddBankCardController calls LoadBankAcountById with correct id

--- src/presentation/controllers/bank-cards/add-bank-card/add-bank-card-controller.ts
+++ src/presentation/controllers/bank-cards/add-bank-card/add-bank-card-controller.ts
@@ -1,10 +1,15 @@
 import { Controller, HttpRequest, HttpResponse } from "@/presentation/protocols"
-import { badRequest, ok, serverError } from "@/presentation/helpers/http/http-helper"
+import { badRequest, notFound, ok, serverError } from "@/presentation/helpers/http/http-helper"
 import { AddBankCard } from "@/domain/use-cases/add-bank-card"
 import { Validation } from "@/presentation/protocols/validation"
+import { LoadBankAccountById } from "@/domain/use-cases/load-bank-account-by-id"
 
 export class AddBankCardController implements Controller {
-    constructor(private readonly addBankCard: AddBankCard, private readonly validation: Validation) {}
+    constructor(
+        private readonly addBankCard: AddBankCard, 
+        private readonly validation: Validation, 
+        private readonly loadBankAccountById: LoadBankAccountById
+    ) {}
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         try {
             const error = this.validation.validate(httpRequest.body)
@@ -13,6 +18,7 @@ export class AddBankCardController implements Controller {
             }
             const { number, flag, expiresAt } = httpRequest.body
             const { bankAccountId } = httpRequest.params
+            const exists = await this.loadBankAccountById.load(bankAccountId)
             const bankCard = await this.addBankCard.add({ number, flag, expiresAt }, bankAccountId)
             return ok(bankCard)
         } catch (error: any) {
