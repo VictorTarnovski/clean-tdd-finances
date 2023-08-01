5a3ef471d85c793552536f318163b326235e67b0
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-08-01 01:41:33 -0300

    feat: ensure DbLoadBankCardById calls LoadBankCardByIdRepository with correct id

---
+++ src/data/use-cases/bank-card/load-bank-card-by-id/db-load-bank-card-by-id.ts
@@ -0,0 +1,12 @@
+import { BankCardModel } from "@/domain/models/bank-card"
+import { LoadBankCardById } from "@/domain/use-cases/bank-card/load-bank-card-by-id"
+import { LoadBankCardByIdRepository } from "@/data/protocols/db/bank-card/load-bank-card-by-id-repository"
+
+export class DbLoadBankCardById implements LoadBankCardById {
+  constructor(private readonly loadBankCardByIdRepository: LoadBankCardByIdRepository) {}
+  async load(bankCardId: string): Promise<BankCardModel | null> {
+    const bankCard = await this.loadBankCardByIdRepository.loadById(bankCardId)
+    if(!bankCard) return null
+    return bankCard
+  }
+}
