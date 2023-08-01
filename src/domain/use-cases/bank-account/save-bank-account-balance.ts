e075e028d25b30520c534fa5d9577b45f6503dca
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-08-01 03:21:02 -0300

    feat: add SaveBankAccountBalance use-case

--- src/domain/use-cases/bank-account/save-bank-account-balance.ts
+++ src/domain/use-cases/bank-account/save-bank-account-balance.ts
@@ -1,5 +1,5 @@
 import { BankAccountModel } from "@/domain/models/bank-account"
 
 export interface SaveBankAccountBalance {
-    save(balance: number): Promise<BankAccountModel>
+    save(balance: number, bankAccountId: string): Promise<BankAccountModel>
 }
