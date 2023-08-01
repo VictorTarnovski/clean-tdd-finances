3f4aff0eff649baeca7721b6179e68176e76d8cc
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-08-01 00:17:37 -0300

    test: ensure DbAddTransaction returns a Transaction on success

--- src/data/use-cases/transaction/add-transaction/db-add-transaction.spec.ts
+++ src/data/use-cases/transaction/add-transaction/db-add-transaction.spec.ts
@@ -1,4 +1,4 @@
-import { mockAddTransactionModel } from "@/domain/tests"
+import { mockAddTransactionModel, mockTransactionModel } from "@/domain/tests"
 import { DbAddTransaction } from "./db-add-transaction"
 import { mockAddTransactionRepository } from "@/data/tests"
 
@@ -18,5 +18,11 @@ describe('DbAddTransaction Usecase', () => {
     const addSpy = jest.spyOn(addTransactionRepository, 'add')
     await sut.add(mockAddTransactionModel())
     expect(addSpy).toBeCalledWith(mockAddTransactionModel())
+  })
+
+  test('Should return a Transaction on success', async () => {
+    const { sut } = makeSut()
+    const transaction = await sut.add(mockAddTransactionModel())
+    expect(transaction).toEqual(mockTransactionModel())
   })
 })
