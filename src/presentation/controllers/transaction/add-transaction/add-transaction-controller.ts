de445b8a421fc341ba9fcf8c6c5801836e578e85
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-08-01 01:55:15 -0300

    feat: ensure AddTransaction Controller calls LoadBankAcountById with correct id

--- src/presentation/controllers/transaction/add-transaction/add-transaction-controller.ts
+++ src/presentation/controllers/transaction/add-transaction/add-transaction-controller.ts
@@ -2,15 +2,19 @@ import { Controller, HttpRequest, HttpResponse, Validation } from "@/presentatio
 import { badRequest, notFound, ok, serverError } from "@/presentation/helpers/http/http-helper"
 import { AddTransaction } from "@/domain/use-cases/transaction/add-transaction"
 import { LoadBankAccountById } from "@/domain/use-cases/bank-account/load-bank-account-by-id"
+import { LoadBankCardById } from "@/domain/use-cases/bank-card/load-bank-card-by-id"
 
 export class AddTransactionController implements Controller {
   constructor(
-    private readonly addTransaction: AddTransaction, private readonly validation: Validation, private readonly loadBanKAccountById: LoadBankAccountById
+    private readonly addTransaction: AddTransaction,
+    private readonly validation: Validation,
+    private readonly loadBankAccountById: LoadBankAccountById,
+    private readonly loadBankCardById: LoadBankCardById
   ) { }
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const error = this.validation.validate(httpRequest.body)
-      if(error) {
+      if (error) {
         return badRequest(error)
       }
       const {
@@ -20,9 +24,12 @@ export class AddTransactionController implements Controller {
         bankAccountId,
         bankCardId
       } = httpRequest.body
-      const exists = await this.loadBanKAccountById.load(bankAccountId)
-      if(!exists) {
+      const bankAccountExists = await this.loadBankAccountById.load(bankAccountId)
+      if (!bankAccountExists) {
         return notFound('bankAccount')
+      }
+      if(bankCardId) {
+        await this.loadBankCardById.load(bankCardId)
       }
       const transaction = await this.addTransaction.add({ description, value, operation, bankAccountId, bankCardId })
       return ok(transaction)
