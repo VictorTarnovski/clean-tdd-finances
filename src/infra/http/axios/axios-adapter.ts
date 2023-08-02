6f4c2fb580d6f9f420121e86fbb4b263890a7952
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-08-02 16:57:55 -0300

    chore: remove axios

--- src/infra/http/axios/axios-adapter.ts
+++
@@ -1,9 +0,0 @@
-import { HttpService, RequestModel, ResponseModel, httpMethods } from "@/infra/http/protocols/http-service"
-
-import axios from 'axios'
-export class AxiosAdapter implements HttpService {
-    async post({ url, requestBody, requestParams, requestHeaders }: RequestModel): Promise<ResponseModel> {
-        const { data, status, headers } = await axios.request({ url, method: httpMethods.POST, data: requestBody, params: requestParams, headers: requestHeaders})
-        return { statusCode: status, headers: headers, body: data }
-    }
-}
