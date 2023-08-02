41c7419c0751a4b06e7f1dc8e9a3c97975e246a0
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-08-02 16:54:01 -0300

    test: ensure DbLoadTransactionsByBankAccount calls LoadTransactionRepositorysByBankAccount with correct id

--- src/domain/tests/mock-transaction.ts
+++ src/domain/tests/mock-transaction.ts
@@ -11,6 +11,25 @@ export const mockTransactionModel = (): TransactionModel => ({
   bankCardId: 'any_bank_card_id',
 })
 
+export const mockTransactionModels = (): TransactionModel[] => ([{
+  id: 'any_transaction_id',
+  description: 'any_description',
+  value: 100,
+  operation: 'addition',
+  createdAt: new Date(),
+  bankAccountId: 'any_bank_account_id',
+  bankCardId: 'any_bank_card_id',
+},
+{
+  id: 'any_transaction_id',
+  description: 'any_description',
+  value: 50,
+  operation: 'subtraction',
+  createdAt: new Date(),
+  bankAccountId: 'any_bank_account_id',
+  bankCardId: 'any_bank_card_id',
+}])
+
 export const mockAddTransactionModel = (): AddTransactionModel => ({
   description: 'any_description',
   value: 100,
