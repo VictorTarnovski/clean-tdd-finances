1d24bb179ceede3d20e615dd9891f005a65d7906
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-08-23 15:42:42 -0300

    feat: ensure LoadCurrencies Controller returns return 500 if LoadCurrencies thorw

--- src/presentation/controllers/currency/load-currencies-controller.ts
+++ src/presentation/controllers/currency/load-currencies-controller.ts
@@ -1,11 +1,15 @@
 import { Controller, HttpResponse } from "@/presentation/protocols"
-import { ok } from "@/presentation/helpers/http/http-helper"
+import { ok, serverError } from "@/presentation/helpers/http/http-helper"
 import { LoadCurrencies } from "@/domain/use-cases"
 
 export class LoadCurrenciesController implements Controller {
   constructor(private readonly loadCurrencies: LoadCurrencies) { }
   async handle(request: any): Promise<HttpResponse> {
-    const currencies = await this.loadCurrencies.load()
-    return ok(currencies)
+    try {
+      const currencies = await this.loadCurrencies.load()
+      return ok(currencies)
+    } catch (error: any) {
+      return serverError(error)
+    }
   }
 }
