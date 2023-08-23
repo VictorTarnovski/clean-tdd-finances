4ac0c0e62d82540c6e2d6eb50de31ee2377a5ea8
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-08-23 15:42:48 -0300

    test: ensure LoadCurrencies Controller returns return 500 if LoadCurrencies thorw

--- tests/presentation/controllers/load-currencies-controller.spec.ts
+++ tests/presentation/controllers/load-currencies-controller.spec.ts
@@ -1,3 +1,4 @@
+import { serverError } from "@/presentation/helpers/http/http-helper"
 import { mockLoadCurrencies } from "../mocks/mock-currency"
 import { LoadCurrenciesController } from "@/presentation/controllers/currency/load-currencies-controller"
 
@@ -16,5 +17,13 @@ describe('LoadCurrencies Controller', () => {
     const loadSpy = jest.spyOn(loadCurenciesStub, 'load')
     await sut.handle({})
     expect(loadSpy).toHaveBeenCalledTimes(1)
+  })
+
+  test('Should return return 500 if LoadCurrencies thorw', async () => {
+    const { sut, loadCurenciesStub } = makeSut()
+    const error = new Error()
+    jest.spyOn(loadCurenciesStub, 'load').mockImplementation( () => { throw error })
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(serverError(error))
   })
 })
