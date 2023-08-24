9638bae2405b7718791ee9da7886cc1c693fbf85
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-08-24 17:11:01 -0300

    feat: ensure MongoBankRepository returns a bank on loadById success

--- src/infra/db/mongodb/mongo-bank-repository.ts
+++ src/infra/db/mongodb/mongo-bank-repository.ts
@@ -1,12 +1,20 @@
-import { LoadBanksRepository } from "@/data/protocols/db"
+import { LoadBankByIdRepository, LoadBanksRepository } from "@/data/protocols/db"
 import { BankModel } from "@/domain/models"
 import mongoHelper from "./mongo-helper"
+import { ObjectId } from "mongodb"
 
-export class MongoBankRepository implements LoadBanksRepository {
+export class MongoBankRepository implements LoadBanksRepository, LoadBankByIdRepository {
   async loadBanks(): Promise<BankModel[]> {
     const bankCollection = await mongoHelper.getCollection('banks')
     const mongoBanks: any = await bankCollection.find().toArray()
     const banks = mongoHelper.mapCollection(mongoBanks)
     return banks
+  }
+
+  async loadBankById(bankId: string): Promise<BankModel | null> {
+    const bankCollection = await mongoHelper.getCollection('banks')
+    const mongoBank = await bankCollection.findOne(new ObjectId(bankId))  
+    const bank = mongoHelper.map(mongoBank)
+    return bank
   }
 }
