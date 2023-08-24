fbbede0821200ad9802f41f88b9d41376cb7b4f7
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-08-24 14:29:09 -0300

    fix: add .toArray() to MongoBankRepository findAll in loadBanks

--- src/infra/db/mongodb/mongo-bank-repository.ts
+++ src/infra/db/mongodb/mongo-bank-repository.ts
@@ -5,7 +5,7 @@ import mongoHelper from "./mongo-helper"
 export class MongoBankRepository implements LoadBanksRepository {
   async loadBanks(): Promise<BankModel[]> {
     const bankCollection = await mongoHelper.getCollection('banks')
-    const mongoBanks: any = await bankCollection.find()
+    const mongoBanks: any = await bankCollection.find().toArray()
     const banks = mongoHelper.mapCollection(mongoBanks)
     return banks
   }
