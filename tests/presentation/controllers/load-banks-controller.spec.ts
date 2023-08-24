fd91513261734cf699acddef6b522db29d9d2e91
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-08-23 21:27:55 -0300

    test: ensure LoadBanks Controller return 500 if loadBanks throws

--- tests/presentation/controllers/load-banks-controller.spec.ts
+++ tests/presentation/controllers/load-banks-controller.spec.ts
@@ -1,5 +1,6 @@
 import { LoadBanksController } from "@/presentation/controllers/bank/load-banks-controller"
 import { mockLoadBanks } from "../mocks"
+import { serverError } from "@/presentation/helpers/http/http-helper"
 
 const makeSut = () => {
   const loadBanksStub = mockLoadBanks()
@@ -10,12 +11,20 @@ const makeSut = () => {
   }
 }  
 describe('LoadBanks Controller', () => {
-  
+
     test('Should call LoadBanks', async () => {
       const { sut, loadBanksStub } = makeSut()
       const loadSpy = jest.spyOn(loadBanksStub, 'load')
       await sut.handle({})
       expect(loadSpy).toHaveBeenCalledTimes(1)
+    })
+
+    test('Should return 500 if loadBanks throw', async () => {
+      const { sut, loadBanksStub } = makeSut()
+      const error = new Error()
+      jest.spyOn(loadBanksStub, 'load').mockImplementationOnce(async () => { throw error })
+      const httpResponse = await sut.handle({})
+      expect(httpResponse).toEqual(serverError(error))
     })
                     
 })
