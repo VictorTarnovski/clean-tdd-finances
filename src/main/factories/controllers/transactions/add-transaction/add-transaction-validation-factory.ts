1a2304246a0de372767524b88c34de68923e7df3
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-10-13 12:53:32 -0300

    fix: fixed add transaction validation

--- src/main/factories/controllers/transactions/add-transaction/add-transaction-validation-factory.ts
+++ src/main/factories/controllers/transactions/add-transaction/add-transaction-validation-factory.ts
@@ -1,12 +1,13 @@
 import { Validation } from "@/presentation/protocols/validation"
 import { ValidationComposite, RequiredFieldValidation, SupportedValuesValidation } from "@/validation/validators"
+import { AddTransactionModel } from "@/domain/use-cases/transaction/add-transaction"
 
 
 export const makeAddTransactionValidation = (): ValidationComposite => {
-  const validations: Validation[] = [] 
-  for (const field of ['description', 'value', 'operation', 'bankAccountId']) {
+  const validations: Validation[] = []
+  for (const field of ['description', 'value', 'type', 'bankAccountId']) {
     validations.push(new RequiredFieldValidation(field))
   }
-  validations.push(new SupportedValuesValidation('operation', ['addition', 'subtraction']))
+  validations.push(new SupportedValuesValidation('type', ['income', 'expense']))
   return new ValidationComposite(validations)
 }
