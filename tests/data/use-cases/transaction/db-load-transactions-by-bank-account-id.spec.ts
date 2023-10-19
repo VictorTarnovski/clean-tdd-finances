f8007a0fd8add5a530b0cb1355be5138a54dc81c
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-10-19 14:07:41 -0300

    test: ensure LoadTransactionByBankAccount Controller integration  with its components

--- tests/data/use-cases/transaction/db-load-transactions-by-bank-account-id.spec.ts
+++ tests/data/use-cases/transaction/db-load-transactions-by-bank-account-id.spec.ts
@@ -18,20 +18,20 @@ describe('DbLoadTransactionsByBankAccountId Usecase', () => {
   test('Should call LoadTransactionsByBankAccountIdRepository with correct bankAccountId', async () => {
     const { sut, loadTransactionsByBankAccountIdRepository } = makeSut()
     const loadByBankAccountIdSpy = jest.spyOn(loadTransactionsByBankAccountIdRepository, "loadByBankAccountId")
-    await sut.loadById('any_bank_account_id')
+    await sut.load('any_bank_account_id')
     expect(loadByBankAccountIdSpy).toHaveBeenCalledWith('any_bank_account_id')
   })
 
   test('Should throw if LoadTransactionsByBankAccountIdRepository throws', async () => {
     const { sut, loadTransactionsByBankAccountIdRepository } = makeSut()
     jest.spyOn(loadTransactionsByBankAccountIdRepository, "loadByBankAccountId").mockImplementationOnce(async () => { throw new Error() })
-    const promise = sut.loadById('any_bank_account_id')
+    const promise = sut.load('any_bank_account_id')
     expect(promise).rejects.toThrow()
   })
 
   test('Should return a Transaction List on success', async () => {
     const { sut } = makeSut()
-    const transactions = await sut.loadById('any_bank_account_id')
+    const transactions = await sut.load('any_bank_account_id')
     expect(transactions).toBeTruthy()
   })
 })
---
+++ tests/presentation/controllers/load-transactions-by-bank-account-id-controller.spec.ts
@@ -0,0 +1,98 @@
+import { LoadTransactionsByBankAccountController } from '@/presentation/controllers/transaction/load-transactions-by-bank-account-controller'
+import { mockLoadBankAccountById, mockLoadTransactionsByBankAccount, mockValidation } from '../mocks'
+import { Validation } from '@/presentation/protocols'
+import { LoadBankAccountById, LoadTransactionsByBankAccount } from '@/domain/use-cases'
+import { badRequest, notFound, ok, serverError } from '@/presentation/helpers/http/http-helper'
+import { mockTransactionModels } from '../../domain/mocks'
+
+const mockRequest = () => ({ bankAccountId: 'any_bank_account_id' })
+
+type SutTypes = {
+  sut: LoadTransactionsByBankAccountController,
+  validation: Validation,
+  loadBankAccountById: LoadBankAccountById,
+  loadTransactionsByBankAccount: LoadTransactionsByBankAccount
+}
+
+const makeSut = (): SutTypes => {
+  const validation = mockValidation()
+  const loadBankAccountById = mockLoadBankAccountById()
+  const loadTransactionsByBankAccount = mockLoadTransactionsByBankAccount()
+  const sut = new LoadTransactionsByBankAccountController(validation, loadBankAccountById, loadTransactionsByBankAccount)
+  return {
+    sut,
+    validation,
+    loadBankAccountById,
+    loadTransactionsByBankAccount
+  } 
+}
+
+describe('LoadTransactionsByBankAccount Controller', () => {
+
+  test('Should call Validation with correct values', async () => {
+    const { sut, validation } = makeSut()
+    const validateSpy = jest.spyOn(validation, 'validate')
+    await sut.handle(mockRequest())
+    expect(validateSpy).toHaveBeenCalledTimes(1)
+    expect(validateSpy).toHaveBeenCalledWith(mockRequest())
+  })
+
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validation } = makeSut()
+    const mockedError = new Error()
+    jest.spyOn(validation, 'validate').mockImplementationOnce(() => { return mockedError })
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(badRequest(mockedError))
+  })
+
+  test('Should return 500 if Validation throws', async () => {
+    const { sut, validation } = makeSut()
+    const mockedError = new Error()
+    jest.spyOn(validation, 'validate').mockImplementationOnce(() => { throw mockedError })
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(serverError(mockedError))
+  })
+
+  test('Should call LoadBankAcountById with correct id', async () => {
+    const { sut, loadBankAccountById } = makeSut()
+    const loadSpy = jest.spyOn(loadBankAccountById, 'load')
+    await sut.handle(mockRequest())
+    expect(loadSpy).toHaveBeenCalledWith('any_bank_account_id')
+  })
+
+  test('Should return 404 if LoadBankAccountById returns null', async () => {
+    const { sut, loadBankAccountById } = makeSut()
+    jest.spyOn(loadBankAccountById, 'load').mockImplementationOnce(async () => null)
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(notFound('bankAccount'))
+  })
+
+  test('Should return 500 if LoadBankAccountById throws', async () => {
+    const { sut, loadBankAccountById } = makeSut()
+    const mockedError = new Error()
+    jest.spyOn(loadBankAccountById, 'load').mockImplementationOnce(() => { throw mockedError })
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(serverError(mockedError))
+  })
+
+  test('Should call LoadTransactionsByBankAccount with correct id', async () => {
+    const { sut, loadTransactionsByBankAccount } = makeSut()
+    const loadSpy = jest.spyOn(loadTransactionsByBankAccount, 'load')
+    await sut.handle(mockRequest())
+    expect(loadSpy).toHaveBeenCalledWith('any_bank_account_id')
+  })
+
+  test('Should return 500 if LoadTransactionsByBankAccount throws', async () => {
+    const { sut, loadTransactionsByBankAccount } = makeSut()
+    const mockedError = new Error()
+    jest.spyOn(loadTransactionsByBankAccount, 'load').mockImplementationOnce(() => { throw mockedError })
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(serverError(mockedError))
+  })
+
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(ok(mockTransactionModels()))
+  })
+})
